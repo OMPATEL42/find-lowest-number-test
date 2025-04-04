73aac0d5b9a69e33b2db97aa0754d4a14f374b2a
Ass-9 <ompatel4246@gmail.com>
2025-04-04 16:38:06 -0400

    Update find_lowest_number.py

--- find_lowest_number.py
+++ find_lowest_number.py
@@ -15,25 +15,28 @@
 
 import sys
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
+def find_lowest_number(input_file, output_file):
+    lowest = None
 
-number_found = False
+    with open(input_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                try:
+                    num = float(line)
+                    if (lowest is None) or (num < lowest):
+                        lowest = num
+                except ValueError:
+                    pass  # Ignore lines that aren't numbers
 
-with open(input_filename, 'r') as input_file:
-    for line in input_file:
-        if number_found == False:
-            try:
-                lowest_number = float(line)
-                number_found = True
-            except ValueError:
-                break
+    with open(output_file, 'w') as out:
+        if lowest is None:
+            out.write("No numbers found in file\n")
         else:
-            if float(line) < lowest_number:
-                lowest_number = float(line)
+            out.write(f"{float(lowest)}\n")
 
-with open(output_filename, 'w') as output_file:
-    if number_found:
-        output_file.write(str(lowest_number) + "\n")
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 find_lowest_number.py <input_file> <output_file>")
     else:
-        output_file.write("No numbers found in file\n")
+        find_lowest_number(sys.argv[1], sys.argv[2])
